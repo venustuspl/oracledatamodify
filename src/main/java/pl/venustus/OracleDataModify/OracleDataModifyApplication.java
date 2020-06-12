b60a28f2abac32c7f1a7d6ef846ed8f25664184e
venustuspl <venustus.pl@gmail.com>
2020-06-12 22:09:57 +0200

    0.0.1 Start

---
+++ src/main/java/pl/venustus/OracleDataModify/OracleDataModifyApplication.java
@@ -0,0 +1,13 @@
+package pl.venustus.OracleDataModify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleDataModifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleDataModifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/venustus/OracleDataModify/OracleDataModifyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.venustus.OracleDataModify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleDataModifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
