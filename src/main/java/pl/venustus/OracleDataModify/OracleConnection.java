b1163a1c7a68cc0e7efbc2c3b6d0c7ab632e1f49
venustuspl <venustus.pl@gmail.com>
2020-07-01 18:48:52 +0200

    1.0.8 Bugs repaired

--- src/main/java/pl/venustus/OracleDataModify/OracleConnection.java
+++ src/main/java/pl/venustus/OracleDataModify/OracleConnection.java
@@ -1,11 +1,13 @@
 package pl.venustus.OracleDataModify;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@Component
 public class OracleConnection {
 
     @Autowired
