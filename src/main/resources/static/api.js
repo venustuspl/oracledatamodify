83737a24be19b891326472aa2b23e9e55cb90feb
venustuspl <venustus.pl@gmail.com>
2020-07-08 22:07:00 +0200

    1.2.8 Creating GUI for marking OR as invoice

--- src/main/resources/static/api.js
+++ src/main/resources/static/api.js
@@ -7,6 +7,8 @@ function append(parent, el) {
     return parent.appendChild(el); // Append the second parameter(element) to the first one
 }
 
+const markedorcount = document.getElementById('markedorcount');
+
 function markorinvoiced(){
 
     var ppe = document.getElementById("ppe").value;
@@ -17,16 +19,6 @@ function markorinvoiced(){
 
 
 fetch(link)
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function (data) {
-        let dates = data; // Get the results
-        return dates.map(function (data) { // Map through the results and for each run the code below
-            let li = createNode('li'), //  Create the elements we need
-                span = createNode('span');
-            li.innerHTML = `data`; // Make the HTML of our span to be the first and last name of our author
-            append(li, span);
-            append(markedorcount, li);
-        })
-
-    });
+  .then(response => response.json())
+  .then(data => document.getElementById('markedorcount').innerHTML = 'dddd');
 }
