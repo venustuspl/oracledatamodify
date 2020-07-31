112fc7807bc942aaac5d564c62448fcb161719e5
venustuspl <venustus.pl@gmail.com>
2020-07-31 22:13:49 +0200

    1.6.2 Added DynamicRollingLogFile.class

---
+++ src/main/java/pl/venustus/OracleDataModify/Service/DynamicRollingLogFile.java
@@ -0,0 +1,34 @@
+package pl.venustus.OracleDataModify.Service;
+
+import org.apache.log4j.DailyRollingFileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+public class DynamicRollingLogFile {
+
+    public static void main(String[] args) {
+        // Creates Pattern Layout
+        PatternLayout patternLayoutObj = new PatternLayout();
+        String conversionPattern = "[%p] %d %c %M - %m%n";
+        patternLayoutObj.setConversionPattern(conversionPattern);
+
+        // Create Daily Rolling Log File Appender
+        DailyRollingFileAppender rollingAppenderObj = new DailyRollingFileAppender();
+        rollingAppenderObj.setFile("sample.log");
+        rollingAppenderObj.setDatePattern("'.'yyyy-MM-dd");
+        rollingAppenderObj.setLayout(patternLayoutObj);
+        rollingAppenderObj.activateOptions();
+
+        // Configure the Root Logger
+        Logger rootLoggerObj = Logger.getRootLogger();
+        rootLoggerObj.setLevel(Level.DEBUG);
+        rootLoggerObj.addAppender(rollingAppenderObj);
+
+        // Create a Customer Logger & Logs Messages
+        Logger loggerObj = Logger.getLogger(DynamicRollingLogFile.class);
+        loggerObj.debug("This is a debug log message");
+        loggerObj.info("This is an information log message");
+        loggerObj.warn("This is a warning log message");
+    }
+}
