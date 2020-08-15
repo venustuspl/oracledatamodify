3e486f10af75524b9e818cf9b4aeb5435531c12d
venustuspl <venustus.pl@gmail.com>
2020-08-15 09:03:09 +0200

    1.6.18 Added kill application script shouldown.bat

--- src/main/java/pl/venustus/OracleDataModify/OracleDataModifyApplication.java
+++ src/main/java/pl/venustus/OracleDataModify/OracleDataModifyApplication.java
@@ -1,12 +1,18 @@
 package pl.venustus.OracleDataModify;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class OracleDataModifyApplication {
     public static void main(String[] args) {
         SpringApplication.run(OracleDataModifyApplication.class, args);
+        SpringApplicationBuilder app = new SpringApplicationBuilder(OracleDataModifyApplication.class)
+                .web(WebApplicationType.NONE);
+        app.build().addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
+        app.run();
     }
-
 }
